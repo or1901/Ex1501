3b2e6d680bfcaa0157e2cdd6d68d42704c5a988a
Ori Roitzaid <oriroitzaid@gmail.com>
2024-02-18 01:28:35 +0200

    Finished defining the alert dialog of the exact alarm - need to find a way to enable it, and also need to add saving of the alarm when the phone reboots

--- app/src/main/java/com/example/ex1501/ExactAlarmReceiver.java
+++ app/src/main/java/com/example/ex1501/ExactAlarmReceiver.java
@@ -1,12 +1,21 @@
 package com.example.ex1501;
 
+import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class ExactAlarmReceiver extends BroadcastReceiver {
 
+    int alarmNum;
     @Override
     public void onReceive(Context context, Intent intent) {
+        alarmNum = intent.getIntExtra("alarmNum", -1);
+
+        MainActivity.getInstance().showAlertDialog(alarmNum);
     }
+
+
 }
--- app/src/main/java/com/example/ex1501/MainActivity.java
+++ app/src/main/java/com/example/ex1501/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.ex1501;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlarmManager;
+import android.app.AlertDialog;
 import android.app.PendingIntent;
 import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
@@ -16,36 +20,43 @@ import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     private AlarmManager alarmMgr;
-    private PendingIntent hourlyAlarmIntent;
-    private int HOURLY_ALARM_REQUEST_CODE = 1;
-    private final Context context = this;
+    private PendingIntent alarmIntent;
+    private final int HOURLY_ALARM_REQUEST_CODE = 1;
+    private int EXACT_ALARM_REQUEST_CODE;
+    private Context context = this;
+    private TextView tvAlarmTime;
+    private AlertDialog.Builder adb;
+    private AlertDialog ad;
+    private final static MainActivity instance = new MainActivity();
+    private Calendar currentAlarmTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        EXACT_ALARM_REQUEST_CODE = 0;
+        tvAlarmTime = (TextView) findViewById(R.id.tvAlarmTime);
         setHourlyAlarm();
     }
 
     public void setHourlyAlarm() {
-        Calendar calNow = Calendar.getInstance();
         Intent intent = new Intent(this, HourlyAlarmReceiver.class);
-        hourlyAlarmIntent = PendingIntent.getBroadcast(this,
+        alarmIntent = PendingIntent.getBroadcast(this,
                 HOURLY_ALARM_REQUEST_CODE, intent, PendingIntent.FLAG_IMMUTABLE);
 
         alarmMgr = (AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
-        alarmMgr.setInexactRepeating(AlarmManager.RTC_WAKEUP,
-                calNow.getTimeInMillis() + 60 * 60 * 1000,
-                AlarmManager.INTERVAL_HOUR, hourlyAlarmIntent);
+        alarmMgr.setInexactRepeating(AlarmManager.ELAPSED_REALTIME,
+                SystemClock.elapsedRealtime() + 60 * 60 * 1000,
+                AlarmManager.INTERVAL_HOUR, alarmIntent);
     }
 
-    private void openTimePickerDialog(boolean is24r) {
+    private void openTimePickerDialog() {
         Calendar calendar = Calendar.getInstance();
 
         TimePickerDialog timePickerDialog = new TimePickerDialog(this, onTimeSetListener,
                 calendar.get(Calendar.HOUR_OF_DAY),
-                calendar.get(Calendar.MINUTE), is24r);
+                calendar.get(Calendar.MINUTE), true);
         timePickerDialog.setTitle("Choose time");
         timePickerDialog.show();
     }
@@ -67,14 +78,67 @@ public class MainActivity extends AppCompatActivity {
                         Toast.LENGTH_LONG).show();
             }
             else {
-
+                setExactAlarm(calSet);
             }
-
         }
     };
 
     public void chooseAlarmTime(View view) {
-        openTimePickerDialog(true);
+        openTimePickerDialog();
+    }
+
+    private void setExactAlarm(Calendar calSet) {
+        Intent intent = new Intent(this, ExactAlarmReceiver.class);
+        intent.putExtra("alarmNum", EXACT_ALARM_REQUEST_CODE);
+
+        alarmIntent = PendingIntent.getBroadcast(this,
+                EXACT_ALARM_REQUEST_CODE, intent, PendingIntent.FLAG_IMMUTABLE);
+        alarmMgr = (AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
+        alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP,
+                calSet.getTimeInMillis(),
+                5 * 60 * 1000, alarmIntent);
+
+        tvAlarmTime.setText("Alarm is set to: " +  calSet.getTime());
+        EXACT_ALARM_REQUEST_CODE++;
+        currentAlarmTime = calSet;
+    }
+
+    public static MainActivity getInstance() {
+        return instance;
+    }
+
+    public void showAlertDialog(int alarmNum) {
+        adb = new AlertDialog.Builder(this);
+        adb.setCancelable(false);
+
+        if(alarmNum == 0) {
+            adb.setTitle("Alarm clock");
+        }
+        else {
+            adb.setTitle("Snooze " + alarmNum);
+        }
+
+        adb.setMessage("Do you want confirm the alarm or set 5 minutes snooze?");
+
+        // Confirm the alarm
+        adb.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                currentAlarmTime.add(Calendar.DATE, 1);
+                EXACT_ALARM_REQUEST_CODE = 0;
+                setExactAlarm(currentAlarmTime);
+            }
+        });
+
+        // Mute and snooze
+        adb.setNegativeButton("Mute & Snooze", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        ad = adb.create();
+        ad.show();
     }
 
     /**
