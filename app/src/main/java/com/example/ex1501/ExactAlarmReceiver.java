db71498bd976eafc1e3a78d700b74ea6ff135400
Ori Roitzaid <oriroitzaid@gmail.com>
2024-02-23 05:29:04 +0200

    Started adding notifications instead of alert dialogs

--- app/src/main/java/com/example/ex1501/ExactAlarmReceiver.java
+++ app/src/main/java/com/example/ex1501/ExactAlarmReceiver.java
@@ -1,5 +1,6 @@
 package com.example.ex1501;
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -12,7 +13,6 @@ import android.content.Intent;
  * @since 19/2/2024
  */
 public class ExactAlarmReceiver extends BroadcastReceiver {
-
     @Override
     public void onReceive(Context context, Intent intent) {
         Intent newActivityIntent = new Intent(context, TransparentDialogActivity.class);
