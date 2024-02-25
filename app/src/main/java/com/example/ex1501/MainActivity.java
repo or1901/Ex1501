144764aadb75ab36c95e7e1774d1059f00dcaf94
Ori Roitzaid <oriroitzaid@gmail.com>
2024-02-25 02:51:04 +0200

    Added documentation to the main activity functions, and made some of the static in order to use them outsize the activity without needing an instance

--- app/src/main/java/com/example/ex1501/MainActivity.java
+++ app/src/main/java/com/example/ex1501/MainActivity.java
@@ -28,13 +28,11 @@ import java.util.Calendar;
  * @since 18/2/2024
  */
 public class MainActivity extends AppCompatActivity {
-    private AlarmManager alarmMgr;
-    private PendingIntent alarmIntent;
-    private final int HOURLY_ALARM_REQUEST_CODE = 1;
-    private TextView tvAlarmTime;
-    private Calendar currentAlarmTime;
-    boolean currentAlarmType;
-    static SharedPreferences spAlarmFile;
+    private static AlarmManager alarmMgr;
+    private static PendingIntent alarmIntent;
+    private final static int HOURLY_ALARM_REQUEST_CODE = -3;
+    private static Calendar currentAlarmTime;
+    private static SharedPreferences spAlarmFile;
     static SharedPreferences.Editor editor;
     private static MainActivity instance;
     private long savedAlarm;
@@ -44,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvAlarmTime = (TextView) findViewById(R.id.tvAlarmTime);
+        instance = this;
 
         spAlarmFile = (SharedPreferences) getSharedPreferences("SAVED_ALARMS", MODE_PRIVATE);
 
@@ -63,10 +61,8 @@ public class MainActivity extends AppCompatActivity {
             if (currentAlarmTime.compareTo(currTime) <= 0) {
                 currentAlarmTime.add(Calendar.DATE, 1);
             }
-            setExactAlarm(currentAlarmTime);
+            setExactAlarm(currentAlarmTime, this);
         }
-
-        instance = this;
     }
 
     /**
@@ -81,8 +77,6 @@ public class MainActivity extends AppCompatActivity {
         alarmMgr.setInexactRepeating(AlarmManager.ELAPSED_REALTIME,
                 SystemClock.elapsedRealtime() + 60 * 60 * 1000,
                 AlarmManager.INTERVAL_HOUR, alarmIntent);
-
-        currentAlarmType = false;
     }
 
     /**
@@ -121,7 +115,7 @@ public class MainActivity extends AppCompatActivity {
             if (calSet.compareTo(calNow) <= 0) {
                 calSet.add(Calendar.DATE, 1);
             }
-            setExactAlarm(calSet);
+            setExactAlarm(calSet, instance);
         }
     };
 
@@ -138,59 +132,59 @@ public class MainActivity extends AppCompatActivity {
      * This function sets an exact alarm to a given calendar time.
      * @param calSet The calendar object to set the alarm to its time
      */
-    public void setExactAlarm(Calendar calSet) {
+    public static void setExactAlarm(Calendar calSet, Context context) {
         int alarmCode = getExactAlarmCode();
+        setExactAlarmCode(alarmCode + 1);
 
-        cancelAlarm(currentAlarmType);  // Cancels the existing alarm
+        cancelAlarm(instance);  // Cancels the existing alarm
         setSnoozeCounter(0);
 
-        Intent intent = new Intent(this, ExactAlarmReceiver.class);
+        Intent intent = new Intent(context, ExactAlarmReceiver.class);
 
-        alarmIntent = PendingIntent.getBroadcast(this,
+        alarmIntent = PendingIntent.getBroadcast(context,
                 alarmCode, intent, PendingIntent.FLAG_IMMUTABLE);
-        alarmMgr = (AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
+        alarmMgr = (AlarmManager)instance.getSystemService(Context.ALARM_SERVICE);
         alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP,
                 calSet.getTimeInMillis(),
-                30 * 1000, alarmIntent);  // Defines with snooze of 5 minutes
+                5 * 60 * 1000, alarmIntent);  // Defines with snooze of 5 minutes
 
-        tvAlarmTime.setText("Alarm is set to: " +  calSet.getTime());
-        setExactAlarmCode(alarmCode + 1);
-        currentAlarmTime = calSet;
-
-        currentAlarmType = true;
-        saveAlarmInFile(currentAlarmTime);
+        saveAlarmInFile(calSet);
     }
 
     /**
-     * This function cancels an existing alarm, according to its type.
-     * @param alarmType true for exact alarm, false for hourly alarm.
+     * This function cancels the current alarm in a given context.
+     * @param context The given context
      */
-    public void cancelAlarm(boolean alarmType) {
+    public static void cancelAlarm(Context context) {
         Intent intent;
+        int alarmCode = getActiveAlarmCode();
 
-        if(!alarmType) {
-            intent = new Intent(this, HourlyAlarmReceiver.class);
-            alarmIntent = PendingIntent.getBroadcast(this,
-                    HOURLY_ALARM_REQUEST_CODE, intent, PendingIntent.FLAG_IMMUTABLE);
+        if(alarmCode > 0) {
+            intent = new Intent(context, HourlyAlarmReceiver.class);
         }
         else {
-            intent = new Intent(this, ExactAlarmReceiver.class);
-            alarmIntent = PendingIntent.getBroadcast(this,
-                    getExactAlarmCode(), intent, PendingIntent.FLAG_IMMUTABLE);
+            intent = new Intent(context, ExactAlarmReceiver.class);
         }
 
-        alarmMgr = (AlarmManager)this.getSystemService(Context.ALARM_SERVICE);
+        alarmIntent = PendingIntent.getBroadcast(context,
+                alarmCode, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        alarmMgr = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
         alarmMgr.cancel(alarmIntent);
     }
 
-    public void setCurrentAlarm(Calendar newAlarm) {
-        currentAlarmTime = newAlarm;
-    }
-
+    /**
+     * This function gets the snooze counter saved in the shared preferences file.
+     * @return The snooze counter saved in the sp file
+     */
     public static int getSnoozeCounter() {
         return spAlarmFile.getInt("snoozeCounter", 0);
     }
 
+    /**
+     * This function sets the snooze counter saved in the shared preferences file.
+     * @param counter The value to set the counter
+     */
     public static void setSnoozeCounter(int counter) {
         editor = spAlarmFile.edit();
 
@@ -198,39 +192,83 @@ public class MainActivity extends AppCompatActivity {
         editor.commit();
     }
 
-    public void saveAlarmInFile(Calendar alarm) {
+    /**
+     * This function saves a given alarm in the shared preferences file.
+     * @param alarm The alarm to save in the file(wil be saved as long type - millis)
+     */
+    public static void saveAlarmInFile(Calendar alarm) {
         editor = spAlarmFile.edit();
 
         editor.putLong("exactAlarm", alarm.getTimeInMillis());
         editor.commit();
     }
 
-    public long getSavedAlarm() {
+    /**
+     * This function gets the alarm saved in the shared preferences file.
+     * @return If exists, the alarm saved in the shared preferences file, otherwise -1
+     */
+    public static long getSavedAlarm() {
         return spAlarmFile.getLong("exactAlarm", -1);
     }
 
-    public void updateCurrentAlarmWithSnooze() {
+    /**
+     * This function updates the current saved alarm with the snooze time.
+     */
+    public static void updateCurrentAlarmWithSnooze() {
         currentAlarmTime = Calendar.getInstance();
         currentAlarmTime.setTimeInMillis(getSavedAlarm());
 
         currentAlarmTime.add(Calendar.MINUTE, 5);
         saveAlarmInFile(currentAlarmTime);
-
-        tvAlarmTime.setText("Alarm is set to: " +  currentAlarmTime.getTime());
     }
 
-    public void setExactAlarmCode(int code) {
+    /**
+     * This function sets the exact alarm code saved in the shared preferences file.
+     * @param code The value to set the current exact alarm code
+     */
+    public static void setExactAlarmCode(int code) {
         editor = spAlarmFile.edit();
 
         editor.putInt("currentAlarmCode", code);
         editor.commit();
     }
-    public int getExactAlarmCode() {
+
+    /**
+     * This function gets the exact alarm code saved in the shared preferences file.
+     * @return The exact alarm cod saved in the sp file
+     */
+    public static int getExactAlarmCode() {
         return spAlarmFile.getInt("currentAlarmCode", 0);
     }
 
+    /**
+     * This function gets the instance of the main activity.
+     * @return The instance of the main activity
+     */
     public static MainActivity getInstance() {
         return instance;
+    }
+
+    /**
+     * This function gets the active alarm code.
+     * @return The active alarm cod
+     */
+    private static int getActiveAlarmCode() {
+        int exactCode = getExactAlarmCode();
+
+        if(exactCode >= 0) {
+            return exactCode;
+        }
+
+        return HOURLY_ALARM_REQUEST_CODE;
+    }
+
+    /**
+     * This function inits the shared preferences file object.
+     * @param context The context to initialize the shared preferences with.
+     */
+    public static void initSpObj(Context context) {
+        spAlarmFile = (SharedPreferences) context.getSharedPreferences("SAVED_ALARMS", MODE_PRIVATE);
     }
 
     /**
